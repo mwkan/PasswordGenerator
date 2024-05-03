f9e1fccd8dad8d7a50b35e34ef3365cad6418206
Mun Wei Kan <munny.kan@hotmail.co.uk>
2024-05-03 15:33:12 +0100

    add about me page

--- passwordgenerator/passwordgenerator/urls.py
+++ passwordgenerator/passwordgenerator/urls.py
@@ -20,4 +20,5 @@ from generator import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('generatedpassword/', views.password, name='password'),
+    path('about/', views.about, name='about'),
 ]
