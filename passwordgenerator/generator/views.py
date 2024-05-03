913d2e8a8ae1065977823f99cfa69b7c6a797322
Mun Wei Kan <munny.kan@hotmail.co.uk>
2024-05-03 13:36:12 +0100

    modify password view to add specialcharacters, numbers, uppercase letters to generated password

--- passwordgenerator/generator/views.py
+++ passwordgenerator/generator/views.py
@@ -11,7 +11,15 @@ def home(request):
 def password(request):
     characters = list('abcdefghijklmnopqrstuvwxyz')
 
-    length = 10
+    length = int(request.GET.get('length', 15))
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+    if request.GET.get('special'):
+        characters.extend(list('!/=*\$%'))
+
     thepassword = ''
     for x in range(length):
         thepassword += random.choice(characters)
